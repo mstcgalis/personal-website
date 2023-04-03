256ea2689bf8a4971cfe2ecfc4b761d421731c06
Daniel Gális <danielgalis21@gmail.com>
2023-04-03 23:29:01 +0200

    first commit

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	"title": "Daniel Gális",
+	"url": "https://danielgalis.com",
+	"language": "en",
+	"description": "Daniel Gális is a designer, researcher and developer working with web technologies.",
+	"author": {
+		"name": "Daniel Gális",
+		"email": "danielgalis21@gmail.com",
+		"url": "https://danielgalis.com"
+	}
+}
