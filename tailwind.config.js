618deeeb5a1c70b1e82ab2013a2c364590122204
Daniel Gális <danielgalis21@gmail.com>
2024-05-01 11:19:57 +0200

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./content/**/*.{html,njk,js}", "./_includes/**/*.njk"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
