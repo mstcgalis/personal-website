1321d2993382e3a8296f74d799e1e753574ec609
Daniel Gális <danielgalis21@gmail.com>
2024-11-07 21:10:22 +0200

    upgrade to 3.0

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,21 +1,26 @@
-const { DateTime } = require("luxon");
-const markdownItAnchor = require("markdown-it-anchor");
-const markdownItFootnote = require("markdown-it-footnote");
-const markdownItObsidianImages = require("markdown-it-obsidian-images")({baseURL: '/posts/attachments/', relativeBaseURL: '/posts/attachments/'});
+import { DateTime } from "luxon";
+import markdownItAnchor from "markdown-it-anchor";
+import markdownItFootnote from "markdown-it-footnote";
+import markdownItObsidianImages from "markdown-it-obsidian-images";
 
-const embedEverything = require("eleventy-plugin-embed-everything");
-const faviconsPlugin = require("eleventy-plugin-gen-favicons");
+const markdownItObsidianImagesConfig = markdownItObsidianImages({
+	baseURL: "/posts/attachments/",
+	relativeBaseURL: "/posts/attachments/",
+});
 
-const pluginRss = require("@11ty/eleventy-plugin-rss");
-const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
-const pluginBundle = require("@11ty/eleventy-plugin-bundle");
-const pluginNavigation = require("@11ty/eleventy-navigation");
-const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
+import embedEverything from "eleventy-plugin-embed-everything";
+import faviconsPlugin from "eleventy-plugin-gen-favicons";
 
-const pluginDrafts = require("./eleventy.config.drafts.js");
-const pluginImages = require("./eleventy.config.images.js");
+import pluginRss from "@11ty/eleventy-plugin-rss";
+import pluginSyntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
+import pluginBundle from "@11ty/eleventy-plugin-bundle";
+import pluginNavigation from "@11ty/eleventy-navigation";
+import { EleventyHtmlBasePlugin } from "@11ty/eleventy";
 
-module.exports = function(eleventyConfig) {
+import pluginDrafts from "./eleventy.config.drafts.js";
+import pluginImages from "./eleventy.config.images.js";
+
+export default function (eleventyConfig) {
 	// Copy the contents of the `public` folder to the output folder
 	// For example, `./public/css/` ends up in `_site/css/`
 	eleventyConfig.addPassthroughCopy({
@@ -23,7 +28,7 @@ module.exports = function(eleventyConfig) {
 		"./node_modules/prismjs/themes/prism-okaidia.css": "/css/prism-okaidia.css",
 		"./content/posts/attachments": "/posts/attachments",
 		"./content/projects/attachments": "/projects/attachments",
-		"./CNAME" : "/CNAME",
+		"./CNAME": "/CNAME",
 	});
 
 	// Run Eleventy when these files change:
@@ -39,7 +44,7 @@ module.exports = function(eleventyConfig) {
 	// Official plugins
 	eleventyConfig.addPlugin(pluginRss);
 	eleventyConfig.addPlugin(pluginSyntaxHighlight, {
-		preAttributes: { tabindex: 0 }
+		preAttributes: { tabindex: 0 },
 	});
 	eleventyConfig.addPlugin(pluginNavigation);
 	eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
@@ -47,44 +52,45 @@ module.exports = function(eleventyConfig) {
 
 	// Other plugins
 	eleventyConfig.addPlugin(embedEverything, {
-		add: ['soundcloud']
-	  });
-	eleventyConfig.addPlugin(faviconsPlugin, {'manifestData': {'name': 'Daniel'}});
-
+		add: ["soundcloud"],
+	});
+	eleventyConfig.addPlugin(faviconsPlugin, {
+		manifestData: { name: "Daniel" },
+	});
 
 	// Filters
 	eleventyConfig.addFilter("readableDate", (dateObj, format, zone) => {
 		// Formatting tokens for Luxon: https://moment.github.io/luxon/#/formatting?id=table-of-tokens
-		return DateTime.fromJSDate(dateObj, { zone: zone || "utc" }).toFormat(format || "dd LLLL yyyy");
+		return DateTime.fromJSDate(dateObj, { zone: zone || "utc" }).toFormat(
+			format || "dd LLLL yyyy"
+		);
 	});
 
-	eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+	eleventyConfig.addFilter("htmlDateString", (dateObj) => {
 		// dateObj input: https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-		return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+		return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
 	});
 
 	// JSON
-	eleventyConfig.addFilter("toJson", function(value) {
+	eleventyConfig.addFilter("toJson", function (value) {
 		return JSON.stringify(value);
-	  });
+	});
 
 	// map pageUrls
 	eleventyConfig.addFilter("mapPageUrls", function (items) {
-		return items.map(item => item.url);
+		return items.map((item) => item.url);
 	});
 	// map tagUrls for Random Tag
 	eleventyConfig.addFilter("mapTagUrls", function (tags) {
-		return tags.map(tag => '/tags/' + tag.toLowerCase());
+		return tags.map((tag) => "/tags/" + tag.toLowerCase());
 	});
-	  
-	  
-  
+
 	// Get the first `n` elements of a collection.
 	eleventyConfig.addFilter("head", (array, n) => {
-		if(!Array.isArray(array) || array.length === 0) {
+		if (!Array.isArray(array) || array.length === 0) {
 			return [];
 		}
-		if( n < 0 ) {
+		if (n < 0) {
 			return array.slice(n);
 		}
 
@@ -97,20 +103,22 @@ module.exports = function(eleventyConfig) {
 	});
 
 	// Return all the tags used in a collection
-	eleventyConfig.addFilter("getAllTags", collection => {
+	eleventyConfig.addFilter("getAllTags", (collection) => {
 		let tagSet = new Set();
-		for(let item of collection) {
-			(item.data.tags || []).forEach(tag => tagSet.add(tag));
+		for (let item of collection) {
+			(item.data.tags || []).forEach((tag) => tagSet.add(tag));
 		}
 		return Array.from(tagSet);
 	});
 
 	eleventyConfig.addFilter("filterTagList", function filterTagList(tags) {
-		return (tags || []).filter(tag => ["all", "nav", "post", "posts", "projects"].indexOf(tag) === -1);
+		return (tags || []).filter(
+			(tag) => ["all", "nav", "post", "posts", "projects"].indexOf(tag) === -1
+		);
 	});
 
 	// Customize Markdown library settings:
-	eleventyConfig.amendLibrary("md", mdLib => {
+	eleventyConfig.amendLibrary("md", (mdLib) => {
 		mdLib.use(markdownItAnchor, {
 			permalink: markdownItAnchor.permalink.ariaHidden({
 				placement: "after",
@@ -118,31 +126,31 @@ module.exports = function(eleventyConfig) {
 				symbol: "#",
 				ariaHidden: false,
 			}),
-			level: [1,2,3,4],
-			slugify: eleventyConfig.getFilter("slugify")
+			level: [1, 2, 3, 4],
+			slugify: eleventyConfig.getFilter("slugify"),
 		});
 		mdLib.use(markdownItFootnote);
 		mdLib.use(markdownItObsidianImages);
 	});
-  
+
 	// Parse tag-URL mapping from text file
 	eleventyConfig.addDataExtension("txt", (contents) => {
-	  const lines = contents.split('\n');
-	  const tagMapping = {};
-	  for (const line of lines) {
-		const [tag, url] = line.split(' ');
-		if (tagMapping[tag]) {
-		  tagMapping[tag].push(url);
-		} else {
-		  tagMapping[tag] = [url];
+		const lines = contents.split("\n");
+		const tagMapping = {};
+		for (const line of lines) {
+			const [tag, url] = line.split(" ");
+			if (tagMapping[tag]) {
+				tagMapping[tag].push(url);
+			} else {
+				tagMapping[tag] = [url];
+			}
 		}
-	  }
-	  return tagMapping;
+		return tagMapping;
 	});
-  
+
 	// Create shortcode for embedding URLs as iFrames
 	eleventyConfig.addShortcode("iframe", (url) => {
-	  return `<iframe src="${url}" width="100%" height="500" loading="lazy"></iframe>`;
+		return `<iframe src="${url}" width="100%" height="500" loading="lazy"></iframe>`;
 	});
 
 	// Features to make your build faster (when you need them)
@@ -156,12 +164,7 @@ module.exports = function(eleventyConfig) {
 	return {
 		// Control which files Eleventy will process
 		// e.g.: *.md, *.njk, *.html, *.liquid
-		templateFormats: [
-			"md",
-			"njk",
-			"html",
-			"liquid",
-		],
+		templateFormats: ["md", "njk", "html", "liquid"],
 
 		// Pre-process *.md files with: (default: `liquid`)
 		markdownTemplateEngine: "njk",
@@ -171,10 +174,10 @@ module.exports = function(eleventyConfig) {
 
 		// These are all optional:
 		dir: {
-			input: "content",          // default: "."
-			includes: "../_includes",  // default: "_includes"
-			data: "../_data",          // default: "_data"
-			output: "_site"
+			input: "content", // default: "."
+			includes: "../_includes", // default: "_includes"
+			data: "../_data", // default: "_data"
+			output: "_site",
 		},
 
 		// -----------------------------------------------------------------
@@ -189,4 +192,4 @@ module.exports = function(eleventyConfig) {
 		// folder name and does **not** affect where things go in the output folder.
 		pathPrefix: "/",
 	};
-};
+}
